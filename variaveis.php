6b9d85b7f35a5ebb75ce661c2291eaa5e7d4d808
mauriciobsantos <mauriciobsantos31@outlook.com>
2019-08-14 22:34:07 -0300

    Variaveis php

---
+++ variaveis.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Variaveis php</title>
+</head>
+<body>
+<?php 
+    $idade = 31;
+    $nome = "Mauricio";
+    echo "$nome tem $idade anos";
+    
+
+?>
+    
+</body>
+</html>
